6f31914725d198a601c27ce298ce0759373fe643
alex122dev <alex122dev@gmail.com>
2023-01-18 17:27:43 +0200

    push to env/development

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo env/development',
+      title: 'Flutter Demo env/development push',
       theme: ThemeData(
         // This is the theme of your application.
         //
