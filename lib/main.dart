6f7405cfbe44b53a436f1d455418c56ac1865c6c
alex122dev <alex122dev@gmail.com>
2023-01-19 11:47:15 +0200

    push env/development test slack

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo env/development push-test',
+      title: 'Flutter Demo env/development push-test slack',
       theme: ThemeData(
         // This is the theme of your application.
         //
